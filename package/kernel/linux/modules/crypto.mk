301416cff77e6529770415933e6f6cf67268fd9e
Eneas U de Queiroz <cotequeiroz@gmail.com>
2019-11-14 10:02:21 -0300

    crypto.mk: add chacha20-poly1305 module
    Signed-off-by: Eneas U de Queiroz <cotequeiroz@gmail.com>

--- package/kernel/linux/modules/crypto.mk
+++ package/kernel/linux/modules/crypto.mk
@@ -93,6 +93,24 @@ endef
 $(eval $(call KernelPackage,crypto-ccm))
 
 
+define KernelPackage/crypto-chacha20poly1305
+  TITLE:=CryptoAPI ChaCha20-Poly1305 AEAD support
+  DEPENDS:=+kmod-crypto-aead
+  KCONFIG:= \
+	CONFIG_CRYPTO_CHACHA20POLY1305 \
+	CONFIG_CRYPTO_CHACHA20 \
+	CONFIG_CRYPTO_POLY1305
+  FILES:= \
+	$(LINUX_DIR)/crypto/chacha20_generic.ko \
+	$(LINUX_DIR)/crypto/poly1305_generic.ko \
+	$(LINUX_DIR)/crypto/chacha20poly1305.ko
+  AUTOLOAD:=$(call AutoLoad,09,chacha20_generic poly1305_generic chacha20poly1305)
+  $(call AddDepends/crypto)
+endef
+
+$(eval $(call KernelPackage,crypto-chacha20poly1305))
+
+
 define KernelPackage/crypto-cmac
   TITLE:=Support for Cipher-based Message Authentication Code (CMAC)
   DEPENDS:=+kmod-crypto-hash
